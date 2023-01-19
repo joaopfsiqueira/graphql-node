59c87bb319ce5e67931bd3ebb138a2a336659b80
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-01-19 17:45:06 -0300

    feat: criando server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
+import { ApolloServer } from 'apollo-server';
 import 'reflect-metadata';
+
+async function bootstrap() {
+  const server = new ApolloServer({});
+
+  const { url } = await server.listen();
+
+  console.log(`🚀  Server ready at ${url}`);
+}
+
+bootstrap();
